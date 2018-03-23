1099bfb5807111f52eec98734a0438c1bb4f5bf6
siulfah <si.ulfah@gmail.com>
2018-03-23 15:39:34 +0700

    komentar percobaan

---
+++ fungsi.py
@@ -0,0 +1,67 @@
+def halo(variabel):
+    print ("selamat %s teman" %variabel)
+halo("siang")
+
+def luas(panjang,lebar):
+    L = panjang*lebar
+    return L
+L=luas(3,4)
+L=4
+print(L)
+
+def prima(bilangan)
+
+    # Meminta input angka batas bawah dan batas atas
+    angka1 = input("Masukkan batas bawah: ")
+    angka2 = input("Masukkan batas atas: ")
+    # membangkitkan bilangan prima
+    if angka1 >= 1 and angka2 > 1:
+        for x in range(angka1, angka2):
+            prima = True
+    for i in range(2, x):
+        if (x % i == 0):
+            prima = False
+    if prima == True:
+        print
+        x
+    else:
+        print
+    "Harus bilangan bulat positif. Tidak boleh negatif dan desimal."
+
+    # include<stdio.h>
+    # include<iostream.h>
+    main()
+    {
+        int
+    bilangan, faktor = 0, a;
+    cout << "Program Bilangan Prima " << endl;
+    cout << "by Fikri" << endl;
+    cout << "====================================" << endl << endl;
+
+    printf("Masukkan bilangan bulat positif: ");
+    scanf("%d", & bilangan);
+
+    for (a=1; a <= bilangan; a++)
+    {
+    if (bilangan % a == 0)
+    {
+    faktor++;
+    }
+    }
+
+    if (faktor == 2)
+    {
+    printf("%d adalah Bilangan Prima.", bilangan);
+    }
+    else
+    {
+    printf("%d bukan Bilangan Prima.", bilangan);
+    }
+    cout << endl;
+    cout << endl;
+    return 0;
+    }
+
+    # def my_function():
+#     print("hello")
+# my_function()

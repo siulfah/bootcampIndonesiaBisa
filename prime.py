9598760e411d44efdae4e4c86420c885ec5c7c6e
siulfah <si.ulfah@gmail.com>
2018-03-23 15:05:40 +0700

    Membuat program menentukan bilangan prima dengan function python

---
+++ prime.py
@@ -0,0 +1,11 @@
+def prime(bilangan):
+    count=0
+    for i in range (1,bilangan+1):
+        if (bilangan % i == 0):
+            count+=1
+    if(count==2):
+        return "prime"
+    else:
+        return "false"
+p = prime(2)
+print (p)
